c4edb49d3587ef50ddb156ebb4bd1760e3e79c0a
Ross Beinhaker <rossbeinhaker@Ross-Beinhakers-MacBook.local>
2015-08-29 09:25:52 -0700

    Initial Commit

---
+++ RNCreateNewFileUtil/RNCreateNewFileUtil.h
@@ -0,0 +1,13 @@
+//
+//  RNCreateNewFileUtil.h
+//  RNCreateNewFileUtil
+//
+//  Created by Ross Beinhaker on 8/29/15.
+//  Copyright (c) 2015 rhaker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNCreateNewFileUtil : NSObject
+
+@end
---
+++ RNCreateNewFileUtil/RNCreateNewFileUtil.m
@@ -0,0 +1,13 @@
+//
+//  RNCreateNewFileUtil.m
+//  RNCreateNewFileUtil
+//
+//  Created by Ross Beinhaker on 8/29/15.
+//  Copyright (c) 2015 rhaker. All rights reserved.
+//
+
+#import "RNCreateNewFileUtil.h"
+
+@implementation RNCreateNewFileUtil
+
+@end
